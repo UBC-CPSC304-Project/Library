5414ab765906cd9d48dcf922db0453abd77fe03f
timch326 <paxwell@outlook.com>
2012-11-08 08:08:59 -0800

    Added new Transaction abstract class, and PayFine class, which inherits it.

---
+++ src/ca/cs304/client/PayFine.java
@@ -0,0 +1,11 @@
+package ca.cs304.client;
+
+public class PayFine extends Transaction {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ca/cs304/client/Transaction.java
@@ -0,0 +1,10 @@
+package ca.cs304.client;
+
+public abstract class Transaction {
+
+	public Transaction() {
+		
+	}
+	
+	public abstract void execute();
+}
