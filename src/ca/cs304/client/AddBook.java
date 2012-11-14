abc862141ee1fd35fd6ebcb15f43e74325d83db1
Brittany Roesch <brroesch@gmail.com>
2012-11-14 13:08:51 -0800

    Add book class, with addbook and findbook methods.  Updated searching for a book.

---
+++ src/ca/cs304/client/AddBook.java
@@ -0,0 +1,82 @@
+package ca.cs304.client;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collection;
+
+import javax.swing.JOptionPane;
+
+public class AddBook extends Transaction {	
+	public AddBook(Connection connection) {
+		super(connection);
+	}
+
+	@Override
+	public Collection<String[]> execute(String[] parameters) {
+		Book book = new Book();
+		PreparedStatement ps;
+		PreparedStatement ps1;
+		try
+		{
+			ps = connection.prepareStatement("INSERT INTO book VALUES ?,?,?,?,?,?)");
+			String callNumber = parameters[0];
+			if (book.findBook(callNumber) == false) {
+				connection.commit();
+			}
+			ps.setString(1, callNumber);
+
+			String isbn= parameters[1];
+			ps.setString(2, isbn);
+
+			String title = parameters[2];
+			ps.setString(3, title);
+		
+			String mainAuthor = parameters[3];
+			ps.setString(4, mainAuthor);
+			
+			String publisher = parameters[4];
+			ps.setString(5, publisher);
+			
+			String year = parameters[5];
+			ps.setString(6, year);
+			
+			ps.executeUpdate();
+			connection.commit();
+			ps.close();
+
+			ps1 = connection.prepareStatement("UPDATE book SET isbn = isbn, title = title, mainAuthor = mainAuthor, publisher = publisher, year = year WHERE callNumber = ?");
+			ps1.setString(1, callNumber);
+			ps1.setString(2, isbn);
+			ps1.setString(3, title);
+			ps1.setString(4, mainAuthor);
+			ps1.setString(5, publisher);
+			ps1.setString(6,  year);
+			ps1.executeUpdate();
+
+			// commit work 
+			connection.commit();
+
+
+			ps1.close();
+		}
+		catch (SQLException ex)
+		{
+			//JOptionPane.showMessageDialog(null,"Message: " + ex.getMessage());
+
+			try 
+			{
+				// undo the insert
+				connection.rollback();	
+
+			}
+			catch (SQLException ex2)
+			{
+				JOptionPane.showMessageDialog(null, "Message: " + ex2.getMessage());
+				System.exit(-1);
+			}
+		}
+
+		return null;
+	}
+}
--- src/ca/cs304/client/Search.java
+++ src/ca/cs304/client/Search.java
@@ -5,97 +5,138 @@ import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+
+import javax.swing.JOptionPane;
 
 
-public class Search {
+public class Search extends Transaction {
 	
-		Book book;
-		private Connection con;
-		
-		public void search(String keyword) {
-			
-			String str1;
+	public Search(Connection connection) {
+		super(connection);
+	}
+
+		@Override
+		public Collection<String[]> execute(String[] parameters) {
 			PreparedStatement ps;
-
-		        try {
-		        	ResultSet rs;
-		            BufferedReader str2 = new BufferedReader(new InputStreamReader(
-		                    System.in));
-		            System.out.println("Enter your search");
-		            str1 = str2.readLine();
-		    		
-		            String query1 = "SELECT * FROM book WHERE title like ?";
-		            ps = con.prepareStatement(query1);
-		            ps.setString(1, str1 + "%");
-		            rs = ps.executeQuery(query1);
-		            while (rs.next()) {
-		              String callNumber = rs.getString("book_callNumber");
-		      	      String isbn = rs.getString("book_isbn");
-		      	      String title = rs.getString("book_title");
-		      	      String mainAuthor = rs.getString("book_mainAuthor");
-		      	      String publisher = rs.getString("book_publisher");
-		      	      String year = rs.getString("book_year");
-		      	      Integer copyNo = rs.getInt("bookcopy_copyno");
-		      	      String status = rs.getString("bookcopy_status");
-		      	     System.out.println(callNumber + "\t" + isbn +
-	                        "\t" + title + "\t" + mainAuthor +
-	                        "\t" + publisher + "\t" + year
-	                        + "\t" + copyNo + "\t" + status);
-		            }
-		            ps.close();
-		            rs.close();
-		            
-		            String query2 = "SELECT * FROM HasAuthor WHERE name like ?";
-		            ps = con.prepareStatement(query2);
-		            ps.setString(1, str1 + "%");
-		            rs = ps.executeQuery(query1);
-		            while (rs.next()) {
-		              String callNumber = rs.getString("book_callNumber");
-		      	      String isbn = rs.getString("book_isbn");
-		      	      String title = rs.getString("book_title");
-		      	      String author = rs.getString("hasauthor_author");
-		      	      String publisher = rs.getString("book_publisher");
-		      	      String year = rs.getString("book_year");
-		      	      Integer copyNo = rs.getInt("bookcopy_copyno");
-		      	      String status = rs.getString("bookcopy_status");
-		      	    System.out.println(callNumber + "\t" + isbn +
-	                        "\t" + title + "\t" + author +
-	                        "\t" + publisher + "\t" + year 
-	                        + "\t" + copyNo + "\t" + status);
-		            }
-		            ps.close();
-		            rs.close();
-		            
-		            String query3 = "SELECT * FROM HasSubject WHERE subject like ?";
-		            ps = con.prepareStatement(query3);
-		            ps.setString(1, str1 + "%");
-		            rs = ps.executeQuery(query1);
-		            while (rs.next()) {
-		              String callNumber = rs.getString("book_callNumber");
-		      	      String isbn = rs.getString("book_isbn");
-		      	      String title = rs.getString("book_title");
-		      	      String mainAuthor = rs.getString("book_mainAuthor");
-		      	      String publisher = rs.getString("book_publisher");
-		      	      String year = rs.getString("book_year");
-		      	      String subject = rs.getString("hassubject_subject");
-		      	      Integer copyNo = rs.getInt("bookcopy_copyno");
-		      	      String status = rs.getString("bookcopy_status");
-		      	    System.out.println(callNumber + "\t" + isbn +
-	                        "\t" + title + "\t" + mainAuthor +
-	                        "\t" + publisher + "\t" + year + "\t" + subject
-	                        + "\t" + copyNo + "\t" + status);
-		            }
-		            ps.close();
-		            rs.close();
-		        }
-		            catch (NullPointerException e)
-
-		            {
-		                System.out.println(e);
-		            } catch (Exception ex) {
-		                System.out.println(ex);
-		            }
+			ResultSet rs;
 			
+			String query1 = "SELECT * FROM book WHERE title like ?";
+			String str1 = parameters[2];
+			try {
+			ps = connection.prepareStatement(query1);
+			ps.setString(1, str1 + "%");
+			rs = ps.executeQuery(query1);
+			while (rs.next()) {
+				String callNumber = rs.getString("book_callNumber");
+				String isbn = rs.getString("book_isbn");
+				String title = rs.getString("book_title");
+				String mainAuthor = rs.getString("book_mainAuthor");
+				String publisher = rs.getString("book_publisher");
+				String year = rs.getString("book_year");
+				Integer copyNo = rs.getInt("bookcopy_copyno");
+				String status = rs.getString("bookcopy_status");
+				System.out.println(callNumber + "\t" + isbn + "\t" + title + "\t" + mainAuthor + 
+						"\t" + publisher + "\t" + year + "\t" + copyNo + "\t" + status);
+			}
+			ps.close();
+			rs.close();
+			}
+			catch (SQLException ex)
+			{
+			    System.out.println("Message: " + ex.getMessage());
+			    
+			    try 
+			    {
+				connection.rollback();	
+			    }
+			    catch (SQLException ex2)
+			    {
+				System.out.println("Message: " + ex2.getMessage());
+				System.exit(-1);
+			    }
+		}
+			
+			
+			
+			String query2 = "SELECT * FROM HasAuthor WHERE name like ?";
+			String str2 = parameters[1];
+			try {
+			ps = connection.prepareStatement(query2);
+			ps.setString(1, str2 + "%");
+			rs = ps.executeQuery(query2);
+			while (rs.next()) {
+				String callNumber = rs.getString("book_callNumber");
+				String isbn = rs.getString("book_isbn");
+				String title = rs.getString("book_title");
+				String author = rs.getString("hasauthor_author");
+				String publisher = rs.getString("book_publisher");
+				String year = rs.getString("book_year");
+				Integer copyNo = rs.getInt("bookcopy_copyno");
+				String status = rs.getString("bookcopy_status");
+				System.out.println(callNumber + "\t" + isbn +
+						"\t" + title + "\t" + author + "\t" + publisher + "\t" + year  + "\t" + copyNo + "\t" + status);
+			}
+			ps.close();
+			rs.close();
+			}
+			catch (SQLException ex)
+			{
+			    System.out.println("Message: " + ex.getMessage());
+			    
+			    try 
+			    {
+				connection.rollback();	
+			    }
+			    catch (SQLException ex2)
+			    {
+				System.out.println("Message: " + ex2.getMessage());
+				System.exit(-1);
+			    }
+		}
+		
+			String query3 = "SELECT * FROM HasSubject WHERE subject like ?";
+			String str3 = parameters[1];
+			try {
+				ps = connection.prepareStatement(query3);
+				ps.setString(1, str3 + "%");
+				rs = ps.executeQuery(query1);
+				while (rs.next()) {
+					String callNumber = rs.getString("book_callNumber");
+					String isbn = rs.getString("book_isbn");
+					String title = rs.getString("book_title");
+					String mainAuthor = rs.getString("book_mainAuthor");
+					String publisher = rs.getString("book_publisher");
+					String year = rs.getString("book_year");
+					String subject = rs.getString("hassubject_subject");
+					Integer copyNo = rs.getInt("bookcopy_copyno");
+					String status = rs.getString("bookcopy_status");
+					System.out.println(callNumber + "\t" + isbn +
+							"\t" + title + "\t" + mainAuthor +
+							"\t" + publisher + "\t" + year + "\t" + subject
+							+ "\t" + copyNo + "\t" + status);
+				}
+				ps.close();
+				rs.close();
+			}
+
+			catch (SQLException ex)
+			{
+			    System.out.println("Message: " + ex.getMessage());
+			    
+			    try 
+			    {
+				connection.rollback();	
+			    }
+			    catch (SQLException ex2)
+			    {
+				System.out.println("Message: " + ex2.getMessage());
+				System.exit(-1);
+			    }
+			}
+			return null;	
 		}
 
-	}
+}
+
