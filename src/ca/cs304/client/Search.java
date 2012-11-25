b7683757c24c7408443fc0f80df056204826e2fa
Brittany Roesch <brroesch@gmail.com>
2012-11-25 14:43:21 -0800

    Merge branch 'master' into britWork

--- src/ca/cs304/client/Search.java
+++ src/ca/cs304/client/Search.java
@@ -24,13 +24,19 @@ public class Search extends Transaction {
 			String subject = parameters.get(2);
 
 			try {
-
-				ps = connection.prepareStatement("SELECT b.callNumber, b.title, COUNT(bc.copyNo) FROM Book b INNER JOIN BookCopy bc ON bc.callNumber = b.callNumber LEFT OUTER JOIN HasSubject s ON b.callNumber = s.callNumber WHERE ((title like '?') OR (mainAuthor like '?') OR (subject like '?')) GROUP BY b.callNumber, b.title");
-//				ps.setString(1, "%");
-//				ps.setString(2, "%");
-//				ps.setString(3, "%");
+				ps = connection.prepareStatement("SELECT b.callNumber, b.title, COUNT(bc.copyNo) FROM Book b INNER JOIN BookCopy bc ON bc.callNumber = b.callNumber LEFT OUTER JOIN HasSubject s ON b.callNumber = s.callNumber WHERE ((title like ?) OR (mainAuthor like ?) OR (subject like ?))  GROUP BY b.callNumber, b.title");
+				ps.setString(1, "%");
+				ps.setString(2, "%");
+				ps.setString(3, "%");
 				rs = ps.executeQuery();
-				
+				while (rs.next()) {
+					if ((rs.getString("title").equalsIgnoreCase(title)) || (rs.getString("author").contains(author)) || (rs.getString("subject").contains(subject))) {
+						System.out.printf(title + author + subject);
+					} else {
+						System.out.printf("no books");
+					}
+				}
+				System.out.printf("no books");
 			}
 			catch (SQLException ex)
 			{
